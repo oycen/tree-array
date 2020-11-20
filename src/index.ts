5043b79d049c3f6bc85274c99922dc9c97451183
欧阳诚 <oycen@sina.com>
2020-11-20 17:32:19 +0800

    add code comment

--- src/index.ts
+++ src/index.ts
@@ -1,32 +1,61 @@
+/** Tree options interface（树选项配置）*/
 export interface TreeOptions {
+  /** Unique identification property name of the tree node（树节点的唯一标识属性名称） */
   id?: string;
+  /** Parent node property name of the tree node（树节点的父节点属性名称） */
   parent?: string;
+  /** Children node property name of the tree node（树节点的子节点属性名称） */
   children?: string;
 }
 
+/** 节点路径 */
+interface NodePath<T> {
+  /** Node index path（节点索引路径） */
+  indexPath: number[];
+  /** Node path（节点路径） */
+  itemPath: T[];
+}
+
+/** Tree Class（树类）*/
 export class Tree<T extends object> {
+  /** Tree default options（树默认选项配置） */
   private static readonly defaultOptions: Required<TreeOptions> = {
     id: 'id',
     parent: 'parent',
     children: 'children',
   };
 
+  /** Tree options（树选项配置） */
   private options: Required<TreeOptions>;
 
+  /** Tree data（树数据） */
   data: T[];
 
+  /**
+   * Tree class constructor（树构造函数）
+   * @param data Tree structure data or flat tree structure data（树形结构数据或扁平的树形结构数据）
+   * @param options Tree options（树选项配置）
+   */
   constructor(data: T[], options?: TreeOptions) {
     this.data = data;
     this.options = { ...Tree.defaultOptions, ...options };
   }
 
-  hasChildren(item: any) {
-    return Array.isArray(item[this.options.children]);
+  /**
+   * Determine node has children（判断节点是否有子节点）
+   * @param node Node object（节点）
+   */
+  hasChildren(node: any) {
+    return Array.isArray(node[this.options.children]) && node[this.options.children].length > 0;
   }
 
+  /**
+   * Tree node traversal（树节点遍历）
+   * @param callback Tree node callback function（节点回调函数）
+   */
   forEach(
-    callback: (this: Tree<T>, value: T, path: { indexPath: number[]; itemPath: T[] }, tree: T[]) => void,
-    path: { indexPath: number[]; itemPath: T[] } = { indexPath: [], itemPath: [] }
+    callback: (this: Tree<T>, node: T, path: NodePath<T>, tree: T[]) => void,
+    path: NodePath<T> = { indexPath: [], itemPath: [] }
   ) {
     this.data.forEach((item: any, index) => {
       try {
@@ -43,9 +72,13 @@ export class Tree<T extends object> {
     });
   }
 
+  /**
+   * Tree node mapping（树节点映射转换）
+   * @param callback Tree node callback function（节点回调函数）
+   */
   map<U>(
-    callback: (this: Tree<T>, value: T, path: { indexPath: number[]; itemPath: T[] }, tree: T[]) => U,
-    path: { indexPath: number[]; itemPath: T[] } = { indexPath: [], itemPath: [] }
+    callback: (this: Tree<T>, node: T, path: NodePath<T>, tree: T[]) => U,
+    path: NodePath<T> = { indexPath: [], itemPath: [] }
   ) {
     return this.data.map((item: any, index) => {
       try {
@@ -63,9 +96,13 @@ export class Tree<T extends object> {
     });
   }
 
+  /**
+   * Tree node filter（树节点过滤）
+   * @param callback Tree node callback function（节点回调函数）
+   */
   filter(
-    callback: (this: Tree<T>, value: T, path: { indexPath: number[]; itemPath: T[] }, tree: T[]) => unknown,
-    path: { indexPath: number[]; itemPath: T[] } = { indexPath: [], itemPath: [] }
+    callback: (this: Tree<T>, node: T, path: NodePath<T>, tree: T[]) => unknown,
+    path: NodePath<T> = { indexPath: [], itemPath: [] }
   ) {
     return this.data.filter((item: any, index) => {
       try {
@@ -83,7 +120,11 @@ export class Tree<T extends object> {
     });
   }
 
-  find(callback: (this: Tree<T>, value: T, path: { indexPath: number[]; itemPath: T[] }, tree: T[]) => unknown) {
+  /**
+   * Find tree node（查找树节点）
+   * @param callback Tree node callback function（节点回调函数）
+   */
+  find(callback: (this: Tree<T>, node: T, path: NodePath<T>, tree: T[]) => unknown) {
     let result: T | undefined;
     try {
       this.forEach((item, path, tree) => {
@@ -97,7 +138,11 @@ export class Tree<T extends object> {
     }
   }
 
-  some(callback: (this: Tree<T>, value: T, path: { indexPath: number[]; itemPath: T[] }, tree: T[]) => unknown) {
+  /**
+   * If there are tree nodes that meet the conditions, it returns true, otherwise it returns false（如有满足条件的树节点则返回true，反之则返回false）
+   * @param callback Tree node callback function（节点回调函数）
+   */
+  some(callback: (this: Tree<T>, node: T, path: NodePath<T>, tree: T[]) => unknown) {
     let result: Boolean = false;
     try {
       this.forEach((item, path, tree) => {
@@ -111,7 +156,11 @@ export class Tree<T extends object> {
     }
   }
 
-  every(callback: (this: Tree<T>, value: T, path: { indexPath: number[]; itemPath: T[] }, tree: T[]) => unknown) {
+  /**
+   * If all tree nodes meet the conditions, it returns true, otherwise it returns false（若所有树节点满足条件则返回true，反之则返回false）
+   * @param callback Tree node callback function（节点回调函数）
+   */
+  every(callback: (this: Tree<T>, node: T, path: NodePath<T>, tree: T[]) => unknown) {
     let result: Boolean = true;
     try {
       this.forEach((item, path, tree) => {
@@ -125,17 +174,28 @@ export class Tree<T extends object> {
     }
   }
 
+  /**
+   * 树扁平化
+   */
   flat() {
     const result: T[] = [];
     this.forEach((item) => result.push(item));
     return result;
   }
 
+  /**
+   * Tree data string representation（树数据字符串表示形式）
+   */
   toString() {
     return JSON.stringify(this.data);
   }
 }
 
+/**
+ * Return a tree instance（返回一个树实例）
+ * @param data Tree structure data or flat tree structure data（树形结构数据或扁平的树形结构数据）
+ * @param options Tree options（树选项配置）
+ */
 export default function tree<T extends object>(data: T[], options?: TreeOptions) {
   return new Tree(data, options);
 }
